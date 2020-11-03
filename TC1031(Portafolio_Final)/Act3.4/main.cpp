6f3b10be9e684cf6fa1f3957d86167f68f50665e
Andres Briseno <72451287+AndresBriC@users.noreply.github.com>
2020-11-02 20:29:41 -0600

    Segunda Prueba del gitignore

--- TC1031(Portafolio_Final)/Act3.4/main.cpp
+++ TC1031(Portafolio_Final)/Act3.4/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include "ConexionesComputadora.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "No deberian estar los archivos indicados por el gitignore";
-
+    cout << "Segunda Prueba del gitignore" << endl;
     return 0;
 }
