b38c631b1f1ef4824a57295ef24baf676af16665
Andres Briseno <72451287+AndresBriC@users.noreply.github.com>
2020-11-02 20:11:53 -0600

    Agrega libreria ConexionesComputadora al main

--- TC1031(Portafolio_Final)/Act3.4/main.cpp
+++ TC1031(Portafolio_Final)/Act3.4/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
+#include "ConexionesComputadora.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Prueba GitHub!" << endl;
-    cout << "Segunda prueba!" << endl;
-    cout << "Tercera prueba" << endl;
+
     return 0;
 }
