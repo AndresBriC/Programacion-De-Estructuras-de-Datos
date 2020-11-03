d7652f54a646c74ddac1ae025f1d0e5ab44e8b04
Andres Briseno <72451287+AndresBriC@users.noreply.github.com>
2020-11-03 08:56:27 -0600

    Prueba 2 de ignore a las carpetas

--- TC1031(Portafolio_Final)/Act3.4/main.cpp
+++ TC1031(Portafolio_Final)/Act3.4/main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main()
 {
-    cout << "Prueba uso de gitbash commit -a, con ignore a bin y obj" << endl;
+    cout << "Prueba uso de gitbash commit -a, con ignore a bin y obj numero 2" << endl;
     return 0;
 }
